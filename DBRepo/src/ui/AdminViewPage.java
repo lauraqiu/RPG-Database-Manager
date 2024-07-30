6702e7154ef2dd2475711ee89329758a644f0126
lauraqiu <lauraqiu21@gmail.com>
2024-07-30 15:34:09 -0700

    implement functionality for admin page

--- DBRepo/src/ui/AdminViewPage.java
+++ DBRepo/src/ui/AdminViewPage.java
@@ -1,7 +1,6 @@
 package ui;
 
 import ui_logic.AdminViewPageDBHandler;
-import ui_logic.CharacterCreationDBHandler;
 
 import javax.swing.*;
 import java.awt.*;
@@ -10,6 +9,7 @@ public class AdminViewPage extends JPanel {
     BackgroundFrame backgroundFrame;
     AdminViewPageDBHandler adminViewPageDBHandler;
     GridBagConstraints gridBagConstraints;
+
     public AdminViewPage(BackgroundFrame backgroundFrame, AdminViewPageDBHandler adminViewPageDBHandler) {
         this.backgroundFrame = backgroundFrame;
         this.adminViewPageDBHandler = adminViewPageDBHandler;
@@ -17,86 +17,126 @@ public class AdminViewPage extends JPanel {
         setLayout(new GridBagLayout());
         gridBagConstraints = new GridBagConstraints();
 
-        JLabel attribute = new JLabel("Attribute View:");
-        JComboBox tableBox = new JComboBox(new String[]{"ACCOUNTS", "CHARACTERS"});
-        JButton searchButton = new JButton("SearchText");
+        JLabel attribute = new JLabel("Select Table:");
+        // add dropdown option for each table
+        JComboBox<String> tableBox = new JComboBox<>(new String[]{
+                "ACCOUNTS",
+                "FRIENDS",
+                "SERVERS",
+                "CHARACTERS",
+                "GLOBALLEADERBOARD",
+                "LOCALLEADERBOARD",
+                "ITEMS",
+                "CONSUMABLES",
+                "EQUIPMENTS",
+                "RESOURCES",
+                "EQUIPPED",
+                "INVENTORY",
+                "SHAREDINVENTORY"
+        });
+        JButton searchButton = new JButton("Search");
 
         gridBagConstraints.gridx = 0;
         gridBagConstraints.gridy = 0;
-
         add(attribute, gridBagConstraints);
 
         gridBagConstraints.gridx = 1;
         add(tableBox, gridBagConstraints);
+
         gridBagConstraints.gridx = 2;
         add(searchButton, gridBagConstraints);
-        searchButton.addActionListener(e -> {
-            String selectedTable = (String) tableBox.getSelectedItem();
-            if (selectedTable.equals("ACCOUNTS")) {
-                JFrame accountFrame = new JFrame();
-                JLabel jLabel = new JLabel("Select Attributes");
-                JCheckBox username = new JCheckBox("UserName");
-                JCheckBox isVerified = new JCheckBox("IsVerified");
-                JCheckBox password = new JCheckBox("Password");
-                JCheckBox email = new JCheckBox("Email");
-                JCheckBox invSlots = new JCheckBox("InvSlots");
-                JButton retrieveData = new JButton("RetrieveData");
 
-                accountFrame.setLayout(new GridBagLayout());
-                accountFrame.add(jLabel, gridBagConstraints);
-                gridBagConstraints.gridy = 1;
-                accountFrame.add(username, gridBagConstraints);
-                gridBagConstraints.gridy = 2;
-                accountFrame.add(isVerified, gridBagConstraints);
-                gridBagConstraints.gridy = 3;
-                accountFrame.add(password, gridBagConstraints);
-                gridBagConstraints.gridy = 4;
-                accountFrame.add(email, gridBagConstraints);
-                gridBagConstraints.gridy = 5;
-                accountFrame.add(invSlots, gridBagConstraints);
-                gridBagConstraints.gridy = 6;
-                accountFrame.add(retrieveData, gridBagConstraints);
+        searchButton.addActionListener(e -> showAttributesFrame((String) tableBox.getSelectedItem()));
+    }
 
-                accountFrame.setVisible(true);
-                accountFrame.setSize(1020,800);
+    // select between tables to display
+    private void showAttributesFrame(String selectedTable) {
+        if ("ACCOUNTS".equals(selectedTable)) {
+            showFrame("ACCOUNTS", new String[]{"username", "isVerified", "password", "email", "InvSlots"});
+        } else if ("FRIENDS".equals(selectedTable)) {
+            showFrame("FRIENDS", new String[]{"username", "friend"});
+        } else if ("SERVERS".equals(selectedTable)) {
+            showFrame("SERVERS", new String[]{"name", "location", "capacity"});
+        } else if ("CHARACTERS".equals(selectedTable)) {
+            showFrame("CHARACTERS", new String[]{"ID", "Acc_User", "name", "class", "age", "height", "weight", "race", "lvl", "money", "strength", "intelligence", "charisma", "dexterity", "luck", "InvSlots", "serverName", "serverLocation"});
+        } else if ("GLOBALLEADERBOARD".equals(selectedTable)) {
+            showFrame("GLOBALLEADERBOARD", new String[]{"id", "Fame", "CID", "Acc_User"});
+        } else if ("LOCALLEADERBOARD".equals(selectedTable)) {
+            showFrame("LOCALLEADERBOARD", new String[]{"id", "Fame", "CID", "Acc_User", "serverLocation", "serverName"});
+        } else if ("ITEMS".equals(selectedTable)) {
+            showFrame("ITEMS", new String[]{"name", "isKey", "description"});
+        } else if ("CONSUMABLES".equals(selectedTable)) {
+            showFrame("CONSUMABLES", new String[]{"itemName", "maxStack"});
+        } else if ("EQUIPMENTS".equals(selectedTable)) {
+            showFrame("EQUIPMENTS", new String[]{"itemName", "type", "strength", "intelligence", "charisma", "dexterity", "luck"});
+        } else if ("RESOURCES".equals(selectedTable)) {
+            showFrame("RESOURCES", new String[]{"itemName", "maxStack"});
+        } else if ("EQUIPPED".equals(selectedTable)) {
+            showFrame("EQUIPPED", new String[]{"EqName", "CID", "Acc_User", "EqType"});
+        } else if ("INVENTORY".equals(selectedTable)) {
+            showFrame("INVENTORY", new String[]{"CID", "Acc_User", "slotNum", "stacks", "itemName"});
+        } else if ("SHAREDINVENTORY".equals(selectedTable)) {
+            showFrame("SHAREDINVENTORY", new String[]{"userID", "slotNum", "stacks", "item"});
+        }
+    }
 
-                retrieveData.addActionListener(t  -> {
-                    StringBuilder query = new StringBuilder();
-                    query.append("SELECT");
-                    boolean userNameSelected = username.isSelected();
-                    boolean isVerifiedSelected = isVerified.isSelected();
-                    boolean passwordSelected = password.isSelected();
-                    boolean emailSelected = email.isSelected();
-                    boolean invSlotsSelected = invSlots.isSelected();
-                    if(userNameSelected) {
-                        query.append(" USERNAME,");
-                    }
-                    if(isVerifiedSelected) {
-                        query.append(" ISVERIFIED,");
-                    }
-                    if(passwordSelected) {
-                        query.append(" PASSWORD,");
-                    }
-                    if(emailSelected) {
-                        query.append(" EMAIL,");
-                    }
-                    if(invSlotsSelected) {
-                        query.append(" INVSLOTS,");
-                    }
-                    //We added an attribute we remove space and comma
-                    if (query.length() > 7){
-                        query.setLength(query.length()- 1);
-                        query.append(" FROM ACCOUNTS");
-                        adminViewPageDBHandler.retrieveData(query.toString(),
-                                userNameSelected, isVerifiedSelected,passwordSelected,
-                                emailSelected, invSlotsSelected);
-                    }
-                    //The user did not select anything
-                    else {
-                        JOptionPane.showMessageDialog(accountFrame, "Error: Did not select any attibutes", "ERROR" , JOptionPane.ERROR_MESSAGE);
-                    }
-                });
+    // to display checkboxes
+    private void showFrame(String tableName, String[] attributes) {
+        JFrame frame = new JFrame();
+        frame.setLayout(new GridBagLayout());
+
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+
+        frame.add(new JLabel("Select"), gbc);
+        gbc.gridy++;
+
+        // add a checkbox for each attribute
+        JCheckBox[] checkBoxes = new JCheckBox[attributes.length];
+        for (int i = 0; i < attributes.length; i++) {
+            checkBoxes[i] = new JCheckBox(attributes[i]);
+            frame.add(checkBoxes[i], gbc);
+            gbc.gridy++;
+        }
+
+        JButton retrieveData = new JButton("Retrieve Data");
+        frame.add(retrieveData, gbc);
+
+        // on click, creates query based on selected attributes
+        retrieveData.addActionListener(e -> {
+            String query = buildQuery(tableName, checkBoxes);
+            if (query != null) {
+                adminViewPageDBHandler.retrieveData(query, checkBoxes);
+            } else {
+                JOptionPane.showMessageDialog(frame, "Error: Did not select any attributes", "ERROR", JOptionPane.ERROR_MESSAGE);
             }
         });
+
+        frame.setVisible(true);
+        frame.setSize(1020, 800);
+    }
+
+    // builds sql string query
+    private String buildQuery(String tableName, JCheckBox[] checkBoxes) {
+        StringBuilder query = new StringBuilder("SELECT ");
+        boolean anySelected = false;
+
+        // append checked attribute to query
+        for (JCheckBox checkBox : checkBoxes) {
+            if (checkBox.isSelected()) {
+                query.append(checkBox.getText()).append(", ");
+                anySelected = true;
+            }
+        }
+
+        // remove space and comma
+        if (anySelected) {
+            query.setLength(query.length() - 2);
+            query.append(" FROM ").append(tableName);
+            return query.toString();
+        }
+
+        return null;
     }
 }
--- DBRepo/src/ui_logic/AdminViewPageDBHandler.java
+++ DBRepo/src/ui_logic/AdminViewPageDBHandler.java
@@ -9,20 +9,16 @@ import java.awt.*;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.Vector;
 
 public class AdminViewPageDBHandler {
-
     DbHandler dbHandler;
 
     public AdminViewPageDBHandler(DbHandler dbHandler) {
         this.dbHandler = dbHandler;
     }
 
-    public void retrieveData(String query, boolean userNameSelected,
-                             boolean isVerifiedSelected, boolean passwordSelected,
-                             boolean emailSelected, boolean invSlotsSelected) {
+    public void retrieveData(String query, JCheckBox[] checkBoxes) {
         try {
             Connection connection = dbHandler.getConnection();
             PrintablePreparedStatement ps = new PrintablePreparedStatement(connection.prepareStatement(query), query, false);
@@ -32,62 +28,45 @@ public class AdminViewPageDBHandler {
             JTable table = new JTable(tableModel);
 
             Vector<String> columnNames = new Vector<>();
-            if (userNameSelected) {
-                columnNames.add("USERNAME");
-            }
-            if (isVerifiedSelected) {
-                columnNames.add("VERIFIED");
-            }
-            if (passwordSelected) {
-                columnNames.add("PASSWORD");
-            }
-            if (emailSelected) {
-                columnNames.add("EMAIL");
-            }
-            if (invSlotsSelected) {
-                columnNames.add("INV_SLOTS");
+            for (JCheckBox checkBox : checkBoxes) {
+                // get column names of selected checkboxes
+                if (checkBox.isSelected()) {
+                    columnNames.add(checkBox.getText().toUpperCase().replace(" ", "_"));
+                }
             }
             tableModel.setColumnIdentifiers(columnNames);
 
+            // populate rows with data returned from query
             while (rs.next()) {
-
-                Vector<Object> vectorList = new Vector<>();
-
-                if (userNameSelected) {
-                    vectorList.add(rs.getString("USERNAME"));
+                Vector<Object> rowData = new Vector<>();
+                for (String columnName : columnNames) {
+                    rowData.add(rs.getObject(columnName));
                 }
-                if (isVerifiedSelected) {
-                    vectorList.add(rs.getString("ISVERIFIED"));
-                }
-                if (passwordSelected) {
-                    vectorList.add(rs.getString("PASSWORD"));
-                }
-                if (emailSelected) {
-                    vectorList.add(rs.getString("EMAIL"));
-                }
-                if (invSlotsSelected) {
-                    vectorList.add(rs.getInt("INVSLOTS"));
-                }
-                tableModel.addRow(vectorList);
+                tableModel.addRow(rowData);
             }
 
-            JScrollPane scrollPane  = new JScrollPane(table);
-            JFrame frame = new JFrame();
+            showResultTable(table);
 
-            frame.setLayout(new GridBagLayout());
-            GridBagConstraints gbc = new GridBagConstraints();
-            gbc.gridx = 0;
-            gbc.gridy = 0;
-            frame.add(scrollPane, gbc);
-            frame.setVisible(true);
-            frame.setSize(1020,800);
             ps.executeUpdate();
             connection.commit();
             ps.close();
-
         } catch (SQLException e) {
             System.out.println("ERROR");
             throw new RuntimeException(e);
         }
+    }
+
+    // moved view table stuff to separate function
+    private void showResultTable(JTable table) {
+        JScrollPane scrollPane = new JScrollPane(table);
+        JFrame frame = new JFrame();
+
+        frame.setLayout(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        frame.add(scrollPane, gbc);
+        frame.setVisible(true);
+        frame.setSize(1020, 800);
     }
 }
